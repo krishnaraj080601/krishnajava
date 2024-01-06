4d69b12fa91ab206e59e41e363a8b892666eb488
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2024-01-07 04:03:03 +0530

    krishnaraj

--- src/krishnaraj/demo.java
+++ src/krishnaraj/demo.java
@@ -1,5 +1,7 @@
 package krishnaraj;
 
 public class demo {
-
+public static void main(String args[]) {
+	System.out.println("krishnaraj")
+}
 }
---
+++ src/krishnaraj/namaste.java
@@ -0,0 +1,5 @@
+package krishnaraj;
+
+public class namaste {
+
+}
